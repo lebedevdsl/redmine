08676f9ef78864838415ced01fb6043fab4b1e22
Alisdair Lebedev <alisdairgm@gmail.com>
2012-03-06 17:27:06 +0400

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Oversun-Scalaxy LTD"
+maintainer_email "d.lebedev@scalaxy.ru"
+license          "Apache 2.0"
+description      "Installs/Configures redmine"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.0.1"
