1be52a965324b10e2c60ba1d43b8c012d4f82eed
Alisdair Lebedev <alisdairgm@gmail.com>
2012-03-10 20:38:50 +0400

    removing some unused strings(addition to "fixed missing hash commas")

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,13 +1,7 @@
 default[:redmine] = {
-  :release_tag => "1.3.1"
-  :app_path => "/var/www/redmine"
   :release_tag => "1.3.1",
   :app_path => "/var/www/redmine",
   :unicorn_conf => {
-    :pid => "/tmp/pids/unicorn.pid" 
-    :sock => "/tmp/socks/unicorn.sock"
-    :error_log => "unicorn.error.log"
-    :access_log => "unicorn.access.log"
     :pid => "/tmp/pids/unicorn.pid", 
     :sock => "/tmp/sockets/unicorn.sock",
     :error_log => "unicorn.error.log",
