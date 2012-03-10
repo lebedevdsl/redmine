6f5c648cb856089b588e8d9da5337eb7ab46c4fc
Alisdair Lebedev <alisdairgm@gmail.com>
2012-03-11 02:16:11 +0400

    Another recipe is needed due to rvm system install

--- recipes/default.rb
+++ recipes/default.rb
@@ -16,7 +16,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-include_recipe 'rvm'
+include_recipe 'rvm::system_install'
 
 packages = node[:redmine][:requirements]
 
