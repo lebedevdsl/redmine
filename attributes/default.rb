414a92fe139f2e2f0e7f2178f6775757e4943185
Alisdair Lebedev <alisdairgm@gmail.com>
2012-03-10 20:35:42 +0400

    fixed missed hash commas

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,11 +1,17 @@
 default[:redmine] = {
   :release_tag => "1.3.1"
   :app_path => "/var/www/redmine"
+  :release_tag => "1.3.1",
+  :app_path => "/var/www/redmine",
   :unicorn_conf => {
     :pid => "/tmp/pids/unicorn.pid" 
     :sock => "/tmp/socks/unicorn.sock"
     :error_log => "unicorn.error.log"
     :access_log => "unicorn.access.log"
+    :pid => "/tmp/pids/unicorn.pid", 
+    :sock => "/tmp/socks/unicorn.sock",
+    :error_log => "unicorn.error.log",
+    :access_log => "unicorn.access.log",
     }
 }
 
