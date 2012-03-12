7d8ac03ce4c70ac2cc83bb0764bfcd82e06476c0
Alisdair Lebedev <alisdairgm@gmail.com>
2012-03-13 01:44:17 +0400

    added dependency for mysql

--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,4 @@ description      "Installs/Configures redmine"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.0.1"
 depends          "rvm"
+depends          "mysql"
